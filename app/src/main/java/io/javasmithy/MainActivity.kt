20eca87f0f96bb41b75207d53a11c69241feb5c4
peter.hydesmith <phydesmi@pio.carrollu.edu>
2019-10-15 15:42:36 -0500

    first test

---
+++ app/src/main/java/io/javasmithy/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.javasmithy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        System.out.println(" HELLO WORLD");
+    }
+}
